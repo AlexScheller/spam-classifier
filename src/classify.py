905109c7a25d78c35015434fdd5d388cae6d3183
Alex <alexgesc@gmail.com>
2018-02-08 20:10:43 -0800

    tests run for spam and non-spam, spam classify rate is abysmal, non-spam classify rate is great. look into this

--- src/classify.py
+++ src/classify.py
@@ -1,3 +1,4 @@
+import os
 import json
 import math
 from zipfile import ZipFile
@@ -54,12 +55,50 @@ def classify(document, model):
 		total_prob = prior_prob + running_word_prob
 		total_prob = -total_prob # cause of log prob
 		# print(json.dumps(doc_class))
-		print(doc_class["class"] + " " + str(total_prob))
+		# print(doc_class["class"] + " " + str(total_prob))
 		if highest_prob is None or total_prob >= highest_prob:
 			highest_prob = total_prob
 			prob_class = doc_class["class"]
 	yield prob_class
 	yield highest_prob
+
+# hard coded for testing purposes
+def test_model(model):
+	num_spam_docs = 0
+	num_total_docs = 0
+	spam_test_path = os.getcwd() + "/../data/spam-test"
+
+	for entry in os.scandir(spam_test_path):
+		if entry.is_file():
+			with open(entry.path, "r") as doc_file:
+				num_total_docs += 1
+				doc = doc_file.readline().split()
+				doc_class, doc_prob = classify(doc, model)
+				if doc_class == "../data/spam":
+					num_spam_docs += 1
+
+	nsd = num_spam_docs
+	ntd = num_total_docs
+	result_string = "{} / {} or {:.2f}%".format(nsd, ntd, (nsd / ntd) * 100)
+	print("total docs correctly identified as spam: " + result_string)
+
+	num_nonspam_docs = 0
+	num_total_docs = 0
+	nonspam_test_path = os.getcwd() + "/../data/nonspam-test"
+
+	for entry in os.scandir(nonspam_test_path):
+		if entry.is_file():
+			with open(entry.path, "r") as doc_file:
+				num_total_docs += 1
+				doc = doc_file.readline().split()
+				doc_class, doc_prob = classify(doc, model)
+				if doc_class == "../data/nonspam":
+					num_nonspam_docs += 1
+
+	nnsd = num_nonspam_docs
+	ntd = num_total_docs
+	result_string = "{} / {} or {:.2f}%".format(nnsd, ntd, (nnsd / ntd) * 100)
+	print("total docs correctly identified as non-spam: " + result_string)
 
 def main():
 
@@ -74,14 +113,16 @@ def main():
 		json_model = json.load(mf)
 
 	model = load_models(json_model)
+
+	test_model(model)
 	# example document
 	# doc_file = "testdoc.txt"
 	# doc_file = "../data/nonspam-test/3-391msg1.txt"
-	doc_file = "../data/spam-test/spmsga138.txt"
+	# doc_file = "../data/spam-test/spmsga138.txt"
 	# doc_file = "test.txt"
-	with open(doc_file, "r") as f:
-		doc = f.readline().split()
-		doc_class, prob = classify(doc, model)
-		print("likely class for {}: {} with (log) prob {}".format(doc_file, doc_class, prob))
+	# with open(doc_file, "r") as f:
+	# 	doc = f.readline().split()
+	# 	doc_class, prob = classify(doc, model)
+	# 	print("likely class for {}: {} with (log) prob {}".format(doc_file, doc_class, prob))
 
 main()
